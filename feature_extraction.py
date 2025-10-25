7dfd89188e45d59e7bc8b4d4c694b6f1e6ffbaf2
Prakruthisb <147960787+Prakruthisb@users.noreply.github.com>
2025-10-25 16:27:56 +0530

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -23,24 +23,25 @@ filenames = pickle.load(open('filenames.pkl','rb'))
 model = VGGFace(model='resnet50',include_top=False,input_shape=(224,224,3),pooling='avg')
 
 # print(model.summary())
-print(len(actors))
+# print(len(actors))
 
-# def feature_extractor(img_path,model):
-#     img = image.load_img(img_path,target_size=(224,224))
-#     img_array = image.img_to_array(img)
-#     expanded_img = np.expand_dims(img_array,axis=0)
-#     preprocessed_img = preprocess_input(expanded_img)
+def feature_extractor(img_path,model):
+    img = image.load_img(img_path,target_size=(224,224))
+    img_array = image.img_to_array(img)
+    expanded_img = np.expand_dims(img_array,axis=0)
+    preprocessed_img = preprocess_input(expanded_img)
 
-#     result = model.predict(preprocessed_img).flatten()
+    result = model.predict(preprocessed_img).flatten()
 
-#     return result
+    return result
 
-# features = []
+features = []
 
-# for file in tqdm(filenames):
-#     features.append(feature_extractor(file,model))
+for file in tqdm(filenames):
+    features.append(feature_extractor(file,model))
 
-# pickle.dump(features,open('embedding.pkl','wb'))
+pickle.dump(features,open('embedding.pkl','wb'))
+
 
 
 
